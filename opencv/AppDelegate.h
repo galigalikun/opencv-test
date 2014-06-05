789cd0d13dae2c3a54be88805e75f35930ab52c0
t_akaishi <t_akaishi@cgate.jp>
2014-06-05 15:13:25 +0900

    Initial Commit

---
+++ opencv/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  opencv
+//
+//  Created by t_akaishi on 2014/06/05.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
